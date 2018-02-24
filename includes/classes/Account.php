241804bd73dc872af6e07365413c93563ce9f2f8
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-24 12:03:35 -0500

    White space and grammar cleanup

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -15,13 +15,11 @@
 			$this->validatePasswords($pw, $pw2);
 
 			if(empty($this->errorArray) == true) {
-				//Insert into db
+				//No error, insert user into database
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
-
 		}
 
 		public function getError($error) {
@@ -31,65 +29,57 @@
 			return "<span class='errorMessage'>$error</span>";
 		}
 
+    //Field validation
+    //===================
 		private function validateUsername($un) {
-
 			if(strlen($un) > 25 || strlen($un) < 5) {
-				array_push($this->errorArray, "Your username must be between 5 and 25 characters");
+				array_push($this->errorArray, "Your username must be between 5 and 25 characters.");
 				return;
 			}
-
 			//TODO: check if username exists
-
 		}
 
 		private function validateFirstName($fn) {
 			if(strlen($fn) > 25 || strlen($fn) < 2) {
-				array_push($this->errorArray, "Your first name must be between 2 and 25 characters");
+				array_push($this->errorArray, "Your first name must be between 2 and 25 characters.");
 				return;
 			}
 		}
 
 		private function validateLastName($ln) {
 			if(strlen($ln) > 25 || strlen($ln) < 2) {
-				array_push($this->errorArray, "Your last name must be between 2 and 25 characters");
+				array_push($this->errorArray, "Your last name must be between 2 and 25 characters.");
 				return;
 			}
 		}
 
 		private function validateEmails($em, $em2) {
 			if($em != $em2) {
-				array_push($this->errorArray, "Your emails don't match");
+				array_push($this->errorArray, "E-Mails do not match.");
 				return;
 			}
-
 			if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
-				array_push($this->errorArray, "Email is invalid");
+				array_push($this->errorArray, "E-Mail address is invalid.");
 				return;
 			}
-
 			//TODO: Check that username hasn't already been used
-
 		}
 
 		private function validatePasswords($pw, $pw2) {
-
 			if($pw != $pw2) {
-				array_push($this->errorArray, "Your passwords don't match");
+				array_push($this->errorArray, "Passwords do not match.");
 				return;
 			}
-
 			if(preg_match('/[^A-Za-z0-9]/', $pw)) {
-				array_push($this->errorArray, "Your password can only contain numbers and letters");
+				array_push($this->errorArray, "Password can only contain numbers and letters.");
 				return;
 			}
-
 			if(strlen($pw) > 30 || strlen($pw) < 5) {
-				array_push($this->errorArray, "Your password must be between 5 and 30 characters");
+				array_push($this->errorArray, "Password must be between 5 and 30 characters.");
 				return;
 			}
-
 		}
-
+  //===================
 
 	}
 ?>
--- register.php
+++ register.php
@@ -31,23 +31,23 @@
 		<form id="registerForm" action="register.php" method="POST">
 			<h2>Register</h2>
 			<p>
-				<?php echo $account->getError("Your username must be between 5 and 25 characters"); ?>
+				<?php echo $account->getError("Your username must be between 5 and 25 characters."); ?>
 				<label for="username">Username</label>
 				<input id="username" name="username" type="text" required>
 			</p>
 			<p>
-				<?php echo $account->getError("Your first name must be between 2 and 25 characters"); ?>
+				<?php echo $account->getError("Your first name must be between 2 and 25 characters."); ?>
 				<label for="firstName">First Name</label>
 				<input id="firstName" name="firstName" type="text" required>
 			</p>
 			<p>
-				<?php echo $account->getError("Your last name must be between 2 and 25 characters"); ?>
+				<?php echo $account->getError("Your last name must be between 2 and 25 characters."); ?>
 				<label for="lastName">Last Name</label>
 				<input id="lastName" name="lastName" type="text" required>
 			</p>
 			<p>
-				<?php echo $account->getError("Your emails don't match"); ?>
-				<?php echo $account->getError("Email is invalid"); ?>
+				<?php echo $account->getError("E-Mails do not match."); ?>
+				<?php echo $account->getError("E-Mail address is invalid."); ?>
 				<label for="email">E-Mail</label>
 				<input id="email" name="email" type="email" required>
 			</p>
@@ -56,9 +56,9 @@
 				<input id="email2" name="email2" type="email" required>
 			</p>
 			<p>
-				<?php echo $account->getError("Your passwords don't match"); ?>
-				<?php echo $account->getError("Your password can only contain numbers and letters"); ?>
-				<?php echo $account->getError("Your password must be between 5 and 30 characters"); ?>
+				<?php echo $account->getError("Passwords do not match."); ?>
+				<?php echo $account->getError("Password can only contain numbers and letters."); ?>
+				<?php echo $account->getError("Password must be between 5 and 30 characters."); ?>
 				<label for="password">Password</label>
 				<input id="password" name="password" type="password" required>
 			</p>
