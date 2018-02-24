cc98dc5a80ba2514aee391be98a90279c1741e50
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-24 17:46:25 -0500

    Memory value of username on login

--- register.php
+++ register.php
@@ -52,7 +52,7 @@
      			<p>
              <?php echo $account->getError(Constants::$loginFailed); ?>
      				<label for="loginUsername">Username</label>
-     				<input id="loginUsername" name="loginUsername" type="text" required>
+     				<input id="loginUsername" name="loginUsername" type="text" value="<?php getInputValue('loginUsername') ?>"required>
      			</p>
      			<p>
      				<label for="loginPassword">Password</label>
