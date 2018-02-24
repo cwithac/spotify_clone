f799f9d53f6c30b436daac83b7572b988b8ef448
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-24 10:44:19 -0500

    Validate names and emails, partial

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -22,19 +22,33 @@
           array_push($this->errorArray, 'Your username must be between 5 and 25 characters.');
           return;
         }
-        //TOOD: Check against user table for username.
+        //TO DO: Check against user table for username.
       }
 
       private function validateFirstName($firstName) {
-
+        if(strlen($firstName) > 25 || strlen($firstName) < 2) {
+          array_push($this->errorArray, 'Your first name must be between 2 and 25 characters.');
+          return;
+        }
       }
 
       private function validateLastName($lastName) {
-
+        if(strlen($lastName) > 25 || strlen($lastName) < 2) {
+          array_push($this->errorArray, 'Your last name must be between 2 and 25 characters.');
+          return;
+        }
       }
 
       private function validateEmails($email, $emailconfirm) {
-
+        if($email != $emailconfirm) {
+          array_push($this->errorArray, 'Your emails do not match.');
+          return;
+        }
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          array_push($this->errorArray, 'Email address is invalid.');
+          return;
+        }
+        //TO DO: Check against table for existing email.
       }
 
       private function validatePasswords($password, $passwordconfirm) {
