7b02c2f9a756b8decdb2c6760ee45c3afa910391
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-25 12:22:54 -0500

    Insert button elements with styling into player control

--- index.php
+++ index.php
@@ -28,7 +28,16 @@
 
         </div>
         <div id="nowPlayingCenter">
-
+          <div class="content playerControls">
+            <div class="buttons">
+              <button class="controlButton shuffle"><img src="assets/images/icons/shuffle.png" title="Shuffle" alt="Shuffle"></button>
+              <button class="controlButton previous"><img src="assets/images/icons/previous.png" title="Previous" alt="Previous"></button>
+              <button class="controlButton play"><img src="assets/images/icons/play-filled.png" title="Play" alt="Play"></button>
+              <button class="controlButton pause" style="display: none;"><img src="assets/images/icons/pause.png" title="Pause" alt="Pause"></button>
+              <button class="controlButton next"><img src="assets/images/icons/next.png" title="Next" alt="Next"></button>
+              <button class="controlButton repeat"><img src="assets/images/icons/repeat.png" title="Repeat" alt="Repeat"></button>
+            </div>
+          </div>
         </div>
         <div id="nowPlayingRight">
 
