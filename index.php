2c3e2d6220044a42327e6fe6f1c8d9639a141de1
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-25 12:06:07 -0500

    Icon upload, initial playing bar setup with CSS

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
   //Require Session/Active Login for page
   include('includes/config.php');
 
+//TEMPORARY SESSION DEACTIVATE
+  // session_destroy();
+
+
   if (isset($_SESSION['userLoggedIn'])) {
     $userLoggedIn = $_SESSION['userLoggedIn'];
   } else {
@@ -14,8 +18,23 @@
   <head>
     <meta charset="utf-8">
     <title>Slotify</title>
+    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-    Hello
+
+    <div id="nowPlayingBarContainer">
+      <div id="nowPlayingBar">
+        <div id="nowPlayingLeft">
+
+        </div>
+        <div id="nowPlayingCenter">
+
+        </div>
+        <div id="nowPlayingRight">
+
+        </div>
+      </div>
+    </div>
+
   </body>
 </html>
