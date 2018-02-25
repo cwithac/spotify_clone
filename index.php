81a4ec32205f5f4a69c8f18b456a5f1eefef0cdc
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-25 16:32:16 -0500

    Access albums from DB

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php include('includes/header.php'); ?>
 
+<h1 class="pageHeadingBig">You Might Also Like</h1>
 
+  <div class="gridViewContainer">
+
+      <?php
+          $albumQuery = mysqli_query($con, "SELECT * FROM albums");
+          while($row = mysqli_fetch_array($albumQuery)) {
+            echo $row['title'] . '<br>';
+          }
+       ?>
+
+  </div>
 
 <?php include('includes/footer.php'); ?>
