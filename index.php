3068c61e400e169b27652136acc54b03d6d87f90
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-25 13:48:06 -0500

    Nav items templating

--- index.php
+++ index.php
@@ -30,6 +30,22 @@
             <a href="index.php" class="logo">
               <img src="assets/images/icons/logo.png" alt="Logo">
             </a>
+            <div class="group">
+              <div class="navItem">
+                <a href="search.php" class="navItemLink">Search</a>
+              </div>
+            </div>
+            <div class="group">
+              <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
+              </div>
+              <div class="navItem">
+                <a href="your-music.php" class="navItemLink">Your Music</a>
+              </div>
+              <div class="navItem">
+                <a href="profile.php" class="navItemLink">User</a>
+              </div>
+            </div>
           </nav>
         </div>
       </div>
