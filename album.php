6488cf7b51440e65a78125a5a7128f462597a34b
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-26 11:15:49 -0500

    GET query for album

--- album.php
+++ album.php
@@ -1,4 +1,19 @@
-<?php include('includes/header.php'); ?>
+<?php include('includes/header.php');
+
+if(isset($_GET['id'])) {
+  $albumId = $_GET['id'];
+} else {
+  header("Location: index.php");
+}
+
+$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
+$album = mysqli_fetch_array($albumQuery);
+
+$artistId = $album['artist'];
+$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id='$artistId'");
+$artist = mysqli_fetch_array($artistQuery);
+
+?>
 
 
 
