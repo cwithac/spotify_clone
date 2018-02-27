ad02d9f760b8f704f0054ee0a1709a8a9073bb20
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-27 10:15:42 -0500

    Time duration updating with correct format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
 var currentPlaylist = [];
 var audioElement;
 
+function formatTime(seconds) {
+	var time = Math.round(seconds);
+	var minutes = Math.floor(time/60);
+	var seconds = time - (minutes * 60);
+
+	var extraZero;
+
+	if(seconds < 10) {
+		extraZero = '0';
+	} else {
+		extraZero = '';
+	}
+
+	return minutes + ':' + extraZero + seconds;
+
+}
+
 function Audio() {
 
 	this.currentlyPlaying;
 	this.audio = document.createElement('audio');
+
+	this.audio.addEventListener('canplay', function() {
+		var duration = formatTime(this.duration);
+		$('.progressTime.remaining').text(duration);
+	});
 
 	this.setTrack = function(track) {
 		this.currentlyPlaying = track;
