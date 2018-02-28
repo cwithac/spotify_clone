4e334b5991d7445ce0250bcb53845b60720f4e3a
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-28 12:46:32 -0500

    Search reset page after two seconds with term

--- search.php
+++ search.php
@@ -12,5 +12,20 @@ if(isset($_GET['term'])) {
 
 <div class="searchContainer">
   <h4>Search for an Artist, Album, or Song</h4>
-  <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing...">
+  <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="var val=this.value; this.value=''; this.value= val;">
 </div>
+
+<script type="text/javascript">
+$('.searchInput').focus(); //Retains focus on search field, onfocus="this.value" ... keeps cursor at end
+  $(function(){
+    var timer;
+    $('.searchInput').keyup(function() {
+      clearTimeout(timer); //Waits until typing has stopped
+      timer = setTimeout(function() {
+        var val = $('.searchInput').val();
+        openPage('search.php?term=' + val);
+        console.log(val);
+      }, 2000);
+    })
+  })
+</script>
