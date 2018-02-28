e7b486131e42ef690142731c9bb1de86dd6e45e0
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-28 18:02:48 -0500

    Playlist templating and light styling

---
+++ yourMusic.php
@@ -0,0 +1,16 @@
+<?php
+
+  include('includes/includedFiles.php');
+
+ ?>
+
+ <div class="playlistContainer">
+   <div class="gridViewContainer">
+     <h2>Playlists</h2>
+     <div class="buttonItems">
+       <button class="button green">
+         NEW PLAYLIST
+       </button>
+     </div>
+   </div>
+ </div>
