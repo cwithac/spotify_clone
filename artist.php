9d3ddf7eb47ba404e6db0dea470a0f2b04696c9c
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-28 11:41:39 -0500

    Artist page styling

--- artist.php
+++ artist.php
@@ -24,6 +24,7 @@ $artist = new Artist($con, $artistId);
 
 
 <div class="trackListContainer borderBottom">
+  <h2>Popular</h2>
   <ul class="tracklist">
     <?php
     $songIdArray = $artist->getSongIds();
@@ -62,4 +63,24 @@ $artist = new Artist($con, $artistId);
       tempPlaylist = JSON.parse(tempSongIds)
     </script>
   </ul>
+</div>
+
+<div class="gridViewContainer">
+<h2>Albums</h2>
+    <?php
+        $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE artist='$artistId'");
+        while($row = mysqli_fetch_array($albumQuery)) {
+          echo "
+          <div class='gridViewItem'>
+          <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+            <img src='" . $row['artworkPath'] . "'>
+            <div class='gridViewInfo'>"
+              . $row['title'] .
+            "</div>
+            </span>
+          </div>
+          ";
+        }
+     ?>
+
 </div>
