495d0527f3da3e752923079e1a9f9e9517ef852f
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-28 18:36:45 -0500

    Output playlist names with user logged in, templating

--- yourMusic.php
+++ yourMusic.php
@@ -12,5 +12,24 @@
          NEW PLAYLIST
        </button>
      </div>
+     <?php
+      $username = $userLoggedIn->getUsername();
+         $playlistQuery = mysqli_query($con, "SELECT * FROM playlist WHERE owner='$username'");
+
+         if(mysqli_num_rows($playlistQuery) == 0) {
+           echo "<span class='noResults'>No playlists found.</span>";
+         }
+
+         while($row = mysqli_fetch_array($playlistQuery)) {
+           echo "
+           <div class='gridViewItem'>
+           <div class='playlistImage'><img src='assets/images/icons/playlist.png'></div>
+             <div class='gridViewInfo'>"
+               . $row['name'] .
+             "</div>
+           </div>
+           ";
+         }
+      ?>
    </div>
  </div>
