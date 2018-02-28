15a24b8697cd5be9416de87b6e6d9b1137bfa7e0
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-27 19:15:32 -0500

    Browse page PHP setup

--- index.php
+++ index.php
@@ -1,30 +1,7 @@
 <?php
-// include('includes/header.php');
 include('includes/includedFiles.php');
 ?>
 
-<h1 class="pageHeadingBig">You Might Also Like</h1>
-
-  <div class="gridViewContainer">
-
-      <?php
-          $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
-          while($row = mysqli_fetch_array($albumQuery)) {
-            echo "
-            <div class='gridViewItem'>
-            <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
-              <img src='" . $row['artworkPath'] . "'>
-              <div class='gridViewInfo'>"
-                . $row['title'] .
-              "</div>
-              </span>
-            </div>
-            ";
-          }
-       ?>
-
-  </div>
-<!-- REMOVED TO PREVENT DUPLICATE AUDIO -->
-<?php
-// include('includes/footer.php');
-?>
+<script type="text/javascript">
+  openPage('browse.php');
+</script>
