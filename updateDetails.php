6553c62ed7f1ef10e075ad7cbb7c3dfb47a8bebf
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-01 17:44:10 -0500

    Settings page templating

---
+++ updateDetails.php
@@ -0,0 +1,22 @@
+<?php
+
+include('includes/includedFiles.php');
+
+ ?>
+
+ <div class="userDetails">
+   <div class="container borderBottom">
+     <h2>EMAIL</h2>
+     <input type="text" class="email" name="email" placeholder="E-Mail Addres..." value="<?php echo $userLoggedIn->getEmail(); ?>">
+     <span class="message"></span>
+     <button class="button" onclick="">SAVE</button>
+   </div>
+   <div class="container">
+     <h2>PASSWORD</h2>
+     <input type="password" class="oldPassword" name="oldPassword" placeholder="Current Password">
+     <input type="password" class="newPassword1" name="newPassword1" placeholder="New Password">
+     <input type="password" class="newPassword2" name="newPassword2" placeholder="Confirm Password">
+     <span class="message"></span>
+     <button class="button" onclick="">SAVE</button>
+   </div>
+ </div>
