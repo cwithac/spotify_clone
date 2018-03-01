30f589658d609c64e4d4adcaebc111be5c80f245
Cathleen Wright <cathleenmwright@gmail.com>
2018-03-01 18:54:01 -0500

    Update Password

--- updateDetails.php
+++ updateDetails.php
@@ -17,6 +17,6 @@ include('includes/includedFiles.php');
      <input type="password" class="newPassword1" name="newPassword1" placeholder="New Password">
      <input type="password" class="newPassword2" name="newPassword2" placeholder="Confirm Password">
      <span class="message"></span>
-     <button class="button" onclick="">SAVE</button>
+     <button class="button" onclick="updatePassword('oldPassword', 'newPassword1', 'newPassword2')">SAVE</button>
    </div>
  </div>
